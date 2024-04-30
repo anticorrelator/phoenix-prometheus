49238217893429effab63a11eab7fe116696000e
Dustin Ngo <dustin@arize.com>
2024-04-29 20:15:58 -0400

    Use better escaping

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sed -e "s|\${POSTGRES_PHOENIX_URL}|$POSTGRES_PHOENIX_URL|g" \
-    -e "s|\${SQLITE_PHOENIX_URL}|$SQLITE_PHOENIX_URL|g" \
+sed -e "s|\POSTGRES_PHOENIX_URL|$POSTGRES_PHOENIX_URL|g" \
+    -e "s|\SQLITE_PHOENIX_URL|$SQLITE_PHOENIX_URL|g" \
     /etc/prometheus/prometheus.yml.template > /etc/prometheus/prometheus.yml
 
 cat /etc/prometheus/prometheus.yml
