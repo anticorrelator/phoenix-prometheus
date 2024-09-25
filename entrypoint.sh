58f8ef38994cb7807ed82010223c5aeb07ace968
Dustin Ngo <dustin@arize.com>
2024-09-25 11:10:55 -0700

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sed -e "s|AUTH_PHOENIX_URL|$AUTH_PHOENIX_URL|g" \
-    -e "s|NOAUTH_PHOENIX_URL|$BASELINE_PHOENIX_URL|g" \
+    -e "s|BASELINE_PHOENIX_URL|$NOAUTH_PHOENIX_URL|g" \
     /etc/prometheus/prometheus.yml.template > /etc/prometheus/prometheus.yml
 
 cat /etc/prometheus/prometheus.yml
