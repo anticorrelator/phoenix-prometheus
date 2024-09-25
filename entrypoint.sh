a242aaa16536a8658838514f548b493426ed9f08
Dustin Ngo <dustin@arize.com>
2024-09-25 11:07:45 -0700

    Use less ambiguous variable names

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 sed -e "s|AUTH_PHOENIX_URL|$AUTH_PHOENIX_URL|g" \
-    -e "s|NOAUTH_PHOENIX_URL|$NOAUTH_PHOENIX_URL|g" \
+    -e "s|NOAUTH_PHOENIX_URL|$BASELINE_PHOENIX_URL|g" \
     /etc/prometheus/prometheus.yml.template > /etc/prometheus/prometheus.yml
 
 cat /etc/prometheus/prometheus.yml
 
 exec /bin/prometheus "$@"
-
