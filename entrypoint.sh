8894c0b2e876070b58a1389b836ae7f395495ad2
Dustin Ngo <dustin@arize.com>
2025-02-20 16:20:47 -0500

    Only one phoenix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-sed -e "s|AUTH_PHOENIX_URL|$AUTH_PHOENIX_URL|g" \
-    -e "s|BASELINE_PHOENIX_URL|$NOAUTH_PHOENIX_URL|g" \
+sed -e "s|PHOENIX_URL|$PHOENIX_URL|g" \
     /etc/prometheus/prometheus.yml.template > /etc/prometheus/prometheus.yml
 
 cat /etc/prometheus/prometheus.yml
